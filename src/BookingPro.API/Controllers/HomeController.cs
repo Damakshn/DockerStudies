2d8f26bff4f910531dad0202c049781c72513a94
Damakshn <damakshon@gmail.com>
2023-04-30 12:09:11 +0800

    Добавлен compose-файл для связывания API и БД,

--- src/BookingPro.API/Controllers/HomeController.cs
+++ src/BookingPro.API/Controllers/HomeController.cs
@@ -5,10 +5,17 @@ namespace BookingPro.API.Controllers
     [Route("/")]
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return Content("Index goes here");
+            return Content($"Index goes here ({_configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT") ?? "undefined"}) mode");
         }
     }
 }
